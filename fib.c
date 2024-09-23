cb28b9a9f36234642b7cc77af6a588b4f29feec6
Patrick Hung <patricksnowah@gmail.com>
2024-09-23 02:08:05 -0700

    pushing my fib2/fib3 project to the branch? or main?

--- fib.c
+++ fib.c
@@ -2,28 +2,41 @@
 #include <stdlib.h>
 //Patrick Hung 860865329
 
-int fIterative(int index);
-int fRecursive(int index);
+// unsigned long long would be much bigger
+long long fib_i(int index);
+long long fib_r(int index);
 
-int main(int argumentLine, char *argumentValue[]) {
+// memoization arrays, 100 should be enough since 92/93 was the cap for signed long long
+// could a pair just work?
+long long memo_i [100];
+long long memo_r [100];
 
-   if (argumentLine != 4)
+int main(int argc, char *argv[]) {
+   //some cleanup
+   char which;
+   long long answer;
+   int argument1;
+   int fileInt;
+   int index;
+
+   //initialize memoization
+   for (int i = 0; i < 100 ; i++)
+   {
+      memo_i[i] = -1;
+      memo_r[i] = -1;
+   }
+
+   if (argc != 4)
    {
       printf("Input error");
       return 1; 
    }
 
-   int argument1 = atoi(argumentValue[1]);
+   argument1 = atoi(argv[1]);
+   argument1 = argument1 - 1;
 
-
-   // * I DONT THINK THIS IS RIGHT BUT I PUT IT IN BECAUSE THE TST CASES FIB INDEX SEEMS TO BE OFF BY 1
-   argument1 = argument1-1;
-
-
-
-   char which = argumentValue[2][0];//[0] is for accessign character in string
-   char *fileName = argumentValue[3];
-
+   which = argv[2][0];//[0] is for accessign character in string, 'i' / 'r'
+   char *fileName = argv[3];
   
 
    FILE *file = fopen(fileName, "r");
@@ -33,50 +46,64 @@ int main(int argumentLine, char *argumentValue[]) {
       return 1;
    }
 
-   int fileInt;
+
    fscanf(file, "%d", &fileInt);
    fclose(file);
 
-   int index = argument1 + fileInt;
+   index = argument1 + fileInt;
 
-   int answer;
-   if(which == 'i') 
-   {
-      answer = fIterative(index);
+   if(which == 'i') {
+      answer = fib_i(index);
    }
-   else{
-      answer = fRecursive(index);
+   else {
+      answer = fib_r(index);
    }
 
-
-   printf("%d", answer);
+   printf("%lld", answer);
 
    return 0;
 }
 
 // iternative
-int fIterative(int index) {
+long long fib_i(int index) {
    if (index <= 0) return 0; 
-   if (index == 1) return 1; 
+   if (index == 1) return 1;
 
-   int a = 0;
-   int b = 1;
-   int temp;
+   if (memo_i[index] != -1 && index < 100) return memo_i[index]; // if memo index exists (not -1 default), return memo pair
+
+   long long a = 0;
+   long long b = 1;
+   long long temp;
+
    for (int i = 2; i <= index; i++) {
       temp = a + b;
       a = b;
       b = temp;
    }
+
+   if (index < 100) { // somewhat redundant bounds checking
+      memo_i[index] = b; // stroign new value in array i
+   }
    return b;
 }
 
 // recursive
-int fRecursive(int index){
+long long fib_r(int index){
    if (index <= 0) return 0;
    if (index == 1) return 1;
 
+   if (memo_r[index] != -1 && index < 100) return memo_r[index];
+   memo_r[index] = fib_r( index - 1) + fib_r(index - 2);// storign new value in r array
+   return memo_r[index];
 
-   return fRecursive(index - 1) + fRecursive(index - 2);
+}
+
+long long fib_wrapper_i (int index) {
+   return fib_i(index);
+}
+
+long long fib_wrapper_r (int index) {
+   return fib_r(index);
 }
 
 
